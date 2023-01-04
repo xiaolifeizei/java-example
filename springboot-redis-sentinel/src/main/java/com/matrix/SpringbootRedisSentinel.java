c9795b5ca65ca39be24737a329b2d1af63391cef
xiaolifeizei <xiaolifeizei@163.com>
2023-01-04 18:17:48 +0800

    Springboot使用Redis哨兵模式的示例

---
+++ springboot-redis-sentinel/src/main/java/com/matrix/SpringbootRedisSentinel.java
@@ -0,0 +1,15 @@
+package com.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : cui_feng
+ * @since : 2023-01-04 18:13
+ */
+@SpringBootApplication
+public class SpringbootRedisSentinel {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisSentinel.class,args);
+    }
+}
